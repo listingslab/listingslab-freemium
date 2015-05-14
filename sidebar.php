8cdcd979033d017eca22b5498340d854f1c20aae
listingslab <listingslab@gmail.com>
2015-05-14 20:11:06 +0930

    vs_4.9.7 → vs_4.9.8

--- sidebar.php
+++ sidebar.php
@@ -5,16 +5,12 @@
 		<a class="laptop" href="?show=menu&action=laptop_click" title="Open the Admin Menu">
 			<img src="<?php print get_template_directory_uri(); ?>/img/png/system/laptop.png" />
 		</a>
-		<a href="/wp-content/plugins/osfrontend-public/" class="btn_osfrontend" title="OSFrontend">
+		<!--<a href="/wp-content/plugins/osfrontend-public/" class="btn_osfrontend" title="OSFrontend">
 			<img src="<?php print get_template_directory_uri(); ?>/img/png/system/osfrontend.png" />
-		</a>
+		</a>-->
 		<a href="?show=search" id="btn_search" title="Search this site">
 			<img src="<?php print get_template_directory_uri(); ?>/img/png/system/search.png" />
 		</a>
-		
-		
-		
-		
 	</div>
 	<?php if ( has_nav_menu( 'primary' ) || is_active_sidebar( 'side_top' )  ) { ?>
 			<?php if (has_nav_menu ('primary')) { ?>
