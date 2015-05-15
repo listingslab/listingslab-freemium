fdc4cfc5ce24f1e9d604d29bbccfba010442ff77
listingslab <listingslab@gmail.com>
2015-05-15 12:17:37 +0930

    vs_4.9.8 → vs_4.9.9

--- php/admin/javascript.php
+++ php/admin/javascript.php
@@ -1,8 +1,7 @@
 <?php $p = get_template_directory_uri().'/js/'; ?>
-<script src='<?php print $p.'jquery-ui/blue/external/jquery/jquery.js'; ?>' type='text/javascript' ></script>
-<script src='<?php print $p.'jquery-ui/blue/jquery-ui.js'; ?>' type='text/javascript' ></script>
+<script src='<?php print $p.'jquery-ui/universal/external/jquery/jquery.js'; ?>' type='text/javascript' ></script>
+<script src='<?php print $p.'jquery-ui/universal/jquery-ui.js'; ?>' type='text/javascript' ></script>
 <script src='<?php print $p.'model.js'; ?>' type='text/javascript' ></script>
-
 <script src='<?php print $p.'view.js'; ?>' type='text/javascript' ></script>
 <script src='<?php print $p.'controller.js'; ?>' type='text/javascript' ></script>
 <script src='<?php print $p.'admin.js'; ?>' type='text/javascript' ></script>
--- php/admin/style.php
+++ php/admin/style.php
@@ -1,3 +1,3 @@
 
-<link href='<?php print get_template_directory_uri(); ?>/js/jquery-ui/listingslab/jquery-ui.css' rel='stylesheet' type='text/css' media='all' />
+<link href='<?php print get_template_directory_uri(); ?>/js/jquery-ui/universal/jquery-ui.css' rel='stylesheet' type='text/css' media='all' />
 <style>#FREEmium_admin {visibility:hidden;}</style>
