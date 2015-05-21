e9d20d3c6ec7778e7e38d529792bcb1db9f052df
listingslab <listingslab@gmail.com>
2015-05-21 16:50:00 +0930

    Remove Docs

--- docs/_nav.php
+++
@@ -1,5 +0,0 @@
-<li><a href="#quickstart">Quick Start Guide</a></li>
-<li><a href="#download">Download &amp; Install</a></li>
-<li><a href="#blogicon">Blog Icon</a></li>
-<li><a href="#multisite_browser">MultiSite Browser</a></li>
-<li><a href="#_artificial_intelligence">Artificial Intelligence</a></li>
