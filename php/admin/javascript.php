7c1073dfa9a630a50d3b9f19bbe531b7ecbd446a
listingslab <listingslab@gmail.com>
2015-05-24 10:56:30 +0930

    Fixes the admin js path problem

--- php/admin/javascript.php
+++ php/admin/javascript.php
@@ -1,6 +1,6 @@
 <?php $p = get_template_directory_uri().'/js/'; ?>
-<script src='<?php print $p.'jquery-ui/universal/external/jquery/jquery.js'; ?>' type='text/javascript' ></script>
-<script src='<?php print $p.'jquery-ui/universal/jquery-ui.js'; ?>' type='text/javascript' ></script>
+<script src='<?php print $p.'jquery-ui/external/jquery/jquery.js'; ?>' type='text/javascript' ></script>
+<script src='<?php print $p.'jquery-ui/jquery-ui.js'; ?>' type='text/javascript' ></script>
 <script src='<?php print $p.'model.js'; ?>' type='text/javascript' ></script>
 <script src='<?php print $p.'view.js'; ?>' type='text/javascript' ></script>
 <script src='<?php print $p.'controller.js'; ?>' type='text/javascript' ></script>
