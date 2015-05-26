a8aa27fd57ad7f170045e364fe82f46d544b96ce
listingslab <listingslab@gmail.com>
2015-05-26 15:55:39 +0930

    Push this version to listingslab

--- php/laptop_menu.php
+++ php/laptop_menu.php
@@ -16,32 +16,25 @@
 	<div id="laptop_menu_left">
 		
 		<h5>Hi <?php print $current_user->display_name; ?>,</h5>
+		<p>Welcome to FREEmium</p>
+		
 		<?php edit_post_link('Edit this content'); ?>
-		
-		
-		<h5>Add Content</h5>
+		<br /><br /><h5>Add Content</h5>
 		<a class="laptop_menu" href="<?php print $adminlink; ?>post-new.php?post_type=page">New Page</a>
 		<a class="laptop_menu" href="<?php print $adminlink; ?>post-new.php">New Post</a>
-		<a class="laptop_menu" href="<?php print $adminlink; ?>post-new.php?post_type=product">New Product</a>
+		<!--<a class="laptop_menu" href="<?php print $adminlink; ?>post-new.php?post_type=product">New Product</a>-->
 		<a class="laptop_menu" href="<?php print $adminlink; ?>import.php">Import</a>
-		
-		
-		
-		
 	</div>
 	<div id="laptop_menu_right">
-		<h5>Site Settings</h5>
-		<a class="laptop_menu" href="<?php print $adminlink; ?>admin.php?page=FREEmium">FREEmium</a>
+		<h5>Manage Site</h5>
 		<a class="laptop_menu" href="<?php print $adminlink; ?>admin.php?page=OSFrontend">OSFrontend</a>
-		
-		<h5>Manage</h5>
+		<a class="laptop_menu" href="<?php print $adminlink; ?>admin.php?page=FREEmium">FREEmium</a>
 		<a class="laptop_menu" href="<?php print $adminlink; ?>customize.php">Customize</a>
 		<a class="laptop_menu" href="<?php print $adminlink; ?>plugins.php">Plugins</a>
 		<a class="laptop_menu" href="<?php print $adminlink; ?>nav-menus.php">Menus</a>
 		<a class="laptop_menu" href="<?php print $adminlink; ?>widgets.php">Widgets</a>
 		<a class="laptop_menu" href="<?php print $adminlink; ?>themes.php">Change Theme</a>
 		<?php print '<a href="'.wp_logout_url().'" class="laptop_menu" title="Logout, '.$current_user->user_login.'" id="gui_logout">Logout</a>'; ?>
-		
 	</div>
 	<?php }else{ wp_login_form();}?>
 	<div style="clear:both; height: 10px;"></div>
