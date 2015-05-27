7e14c7c3b13c862d47dcad0dd5504e182ceb1b70
listingslab <listingslab@gmail.com>
2015-05-27 10:10:14 +0930

    Much better preloader animations

--- footer.php
+++ footer.php
@@ -14,7 +14,30 @@
 <?php wp_footer(); ?>
 </div>
 <div id="preloader">
-	<img src="<?php print get_template_directory_uri(); ?>/img/gif/ajax-loader.gif" />
+	<!-- http://preloaders.net -->
+	<?php 		
+		$animations = array(
+			"loader_apple.GIF",
+			"loader_apple.GIF"
+		);
+		//"loader_android.GIF",
+		//"loader_apple.GIF",
+		//"loader_camera.GIF", 
+		//"loader_cat.GIF", 
+		//"loader_coffee.GIF",
+		//"loader_facebook.GIF", 
+		//"loader_fancypants.GIF",
+		//"loader_foot.GIF", 
+		//"loader_heart.GIF", 
+		//"loader_mail.GIF", 
+		//"loader_skype.GIF", 
+		//"loader_twitter.GIF"
+		$rand_keys = array_rand($animations, 2);
+		$random_animation = $animations[$rand_keys[0]] . "\n";
+	?>
+	<a href="?click=heartbeat">
+	<img src="<?php print get_template_directory_uri(); ?>/img/gif/<?php print $random_animation; ?>" />
+	</a>
 </div>
 </body>
 </html>
